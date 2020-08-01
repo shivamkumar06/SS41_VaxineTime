d6de60aeb243559cab535257695cf92c6bdfc156
Shivam Kumar <shivam.saha06@gmail.com>
2020-08-01 20:54:44 +0530

    User Schema Completed

--- model/User.js
+++ model/User.js
@@ -1,5 +1,17 @@
 const mongoose = require('mongoose')
 
+function formatDate(date) {
+    var d = new Date(date),
+     month = '' + (d.getMonth() + 1),
+     day = '' + d.getDate(),
+     year = d.getFullYear();
+
+    if(month.length < 2) month = '0' + month;
+    if(day.length < 2) day = '0' + day;
+
+    return [day,month,year].join('-');
+}
+
 const userSchema = new mongoose.Schema({
     cname: {
         type: String,
@@ -18,7 +30,7 @@ const userSchema = new mongoose.Schema({
     },
     dateVac: {
         type: Date,
-        //date formatting for later
+        set : date => formatDate(date)
     },
     city: {
         type: String,
@@ -35,8 +47,20 @@ const userSchema = new mongoose.Schema({
     phoneNO: {
         type: Number,
     },
+    coverImage: {
+        type: Buffer,
+    },
+    coverImageType: {
+        type: String,
+    },
     vaxine: [],
 
+})
+
+userSchema.virtual('coverImagePath').get(function(){
+    if(this.coverImage != null && this.coverImage != mull){
+        return `data:${this.coverImageType};charset = utf-8; base64, ${this.coverImage.toString('base64')}` 
+    }
 })
 
 module.exports = mongoose.model('User', userSchema)
