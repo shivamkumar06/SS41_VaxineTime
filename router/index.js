a8e473aa6a520d97bd8fcd9091493958db74a591
Shivam Kumar <shivam.saha06@gmail.com>
2020-08-02 21:45:08 +0530

    Vaccination Post and Get

--- router/index.js
+++ router/index.js
@@ -4,9 +4,96 @@ require('../db/mongoose')
 const User = require('../model/User')
 const {ensureAuthenticated} = require('../config/auth')
 const qr = require('qrcode')
-
+const hospital = require('../model/hospital')
 
 const ImageMimeType = ['image/jpeg','image/png','image/gif']
+
+
+router.get('/vaccine', ensureAuthenticated, async (req, res) => {
+    try {
+      const hospital = await Hospital.find({})
+      const user = await User.find({});
+      const done = await Done.find({ user: req.user.id });
+      console.log(hospital)
+  
+      res.render("vaccine", {
+        user: req.user,
+        done,
+        hospital
+      })
+  
+    }catch (err) {
+      console.log(err)
+    }
+  })
+  
+  router.post('/vaccine', ensureAuthenticated, async (req, res) => {
+  
+    var { vaccineName, date, hospitalName, City1, State1 } = req.body
+  
+    //  date = date.join('')
+    
+    const done = await Done.find({ user: req.user.id })
+    const hospital = await Hospital.find({})
+  
+  
+    // var dateString = date;
+  
+  
+  
+  
+  
+    // hospitalName = hospitalName.join("")
+    // City1 = City1.join("")
+    // State1 = State1.join("")
+    // req.body = { vaccineName, date, hospitalName, City1, State1 }
+    
+    // console.log(req.body)
+  
+    var err = "";
+    user = req.user.id;
+  
+    // console.log(req.user)
+  
+    if (!vaccineName || !date || !hospitalName || !City1 || !State1) {
+      err = "please fill in all the fields!!"
+      res.render("vaccine", {
+        err : err,
+        user: req.user,
+        done,
+        hospital
+      });
+    }else{
+      Done.findOne({ vaccineName: vaccineName, user: user })
+      .then((vaxine) => {
+        if (!vaxine) {
+          req.body.user = req.user.id;
+          Done.create(req.body)
+            .then(() => {
+              res.redirect("/vaccine");
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        } else {
+          err = vaxine.vaccineName + " Vaccine is already applied!!";
+          res.render("vaccine", {
+            err: err,
+            user: req.user,
+            done,
+            hospital
+          });
+        }
+      })
+        .catch((err) => {
+          console.log(err)
+        });
+      } 
+      
+   
+  });
+  
+
 
 router.get('/profile',ensureAuthenticated,async(req,res)=>{
     try{
@@ -83,6 +170,10 @@ function saveCover(user,coverEncoded){
 router.get('/info',(req,res)=>{
     res.render('info')
 })
+
+router.get('*', (req, res) => {
+    res.render('404')
+  })
 
 
 module.exports = router
