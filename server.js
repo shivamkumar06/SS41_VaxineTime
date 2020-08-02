b1b81a71e7d8f666826d6a0d9cc1e46c278b858e
vikas990 <vikaskumarp66@gmail.com>
2020-08-02 08:12:58 +0530

    index page complete

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
+const path = require('path')
 const express = require('express');
+const layouts = require('express-ejs-layouts')
 
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT
 
-app.get('/',(req,res)=>{
-    res.send('Hello World!!');
-})
+
+
+//public dir
+const PublicDir = path.join(__dirname+'/public')
+app.use(express.static(PublicDir))
+
+//Ejs setup
+app.set('view engine','ejs')
+app.set('views','./views')
+
+//layouts
+ app.use(layouts)
+
+
+//router
+app.use(require('./router/user'))
+
+
 
 app.listen(port,()=>console.log(`server up on ${port}`))
